998a299239369a30e4fe446e50482c2cd23a823d
j12k14a <muhammadjawsyan2@gmail.com>
2026-06-24 22:38:50 +0700

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -1,16 +1,16 @@
-from google import genai
-import PIL.Image
+from google import genai # SDK Google Gemini
+import PIL.Image # Library pembaca gambar
 
 # 1. PENTING: Jangan lupa isi ulang API Key Anda di sini
 API_KEY = "ISI dengan Ur API KEY"
 client = genai.Client(api_key=API_KEY)
 
-class PakarBotaniSession:
-    def __init__(self):
+class PakarBotaniSession: # Class yang bertugas mengelola komunikasi dengan Gemini AI
+    def __init__(self): # Method konstruktor Dipanggil otomatis saat object baru dibuat dari sebuah class dan digunakan untuk menetapkan nilai awal
         # Membuat sesi obrolan (chat) agar AI memiliki daya ingat 
-        self.chat = client.chats.create(model='gemini-2.5-flash')
+        self.chat = client.chats.create(model='gemini-2.5-flash') # Membuat sesi chat baru yang memiliki memori percakapan.
         
-    def predict_disease(self, image_path):
+    def predict_disease(self, image_path): # Method utama untuk menganalisis tanaman.
         try:
             img = PIL.Image.open(image_path)
             
@@ -25,15 +25,15 @@ class PakarBotaniSession:
 
             # Kirim gambar dan prompt awal ke memori chat
             response = self.chat.send_message([prompt_text, img])
-            return response.text
+            return response.text # Mengembalikan hasil analisis ke GUI.
 
-        except Exception as e:
-            return f"Terjadi kesalahan saat menghubungi server Google: {str(e)}"
+        except Exception as e:  # Menangkap semua jenis error yang mungkin terjadi  # str(e) digunakan untuk mengubah objek error menjadi teks  
+            return f"Terjadi kesalahan saat menghubungi server Google: {str(e)}" # agar bisa ditampilkan ke pengguna
             
     def ask_follow_up(self, question):
         try:
             # Karena ini bagian dari memori chat, kita cukup mengirimkan teks pertanyaan tambahannya
             response = self.chat.send_message(question)
-            return response.text
+            return response.text 
         except Exception as e:
             return f"Error: {str(e)}"
